4a130aba62f71b954cff2c7559f31ca5c04ecf17
rdy24 <raflizardeswandiy24@gmail.com>
2022-04-12 11:21:36 +0700

    git init & push repository

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const cardDestinations = [...document.querySelectorAll(".card-destination")];
+const parent = [...document.querySelectorAll(".parent")];
+const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
+
+cardDestinations.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
+
+parent.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
